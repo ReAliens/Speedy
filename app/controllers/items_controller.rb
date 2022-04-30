abeac28c3511f2381009aeda60f1db76078cb7ff
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-04-30 10:37:29 +0200

    Update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,60 @@
 class ItemsController < ApplicationController
+  # before_action :set_item, only: %i[show edit update destroy]
+
   def index
     @items = Item.all
+    render json: @items
   end
+
+  def show; end
 
   def new
     @item = Item.new
   end
 
+  def edit; end
+
   def create
-    @item = Item.new(set_item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        # render json: @item
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
     end
   end
 
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        render json: @item
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /items/1
+  # DELETE /items/1.json
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.json { head :no_content }
+    end
   end
 
   private
 
-  def set_item_params
-    params.require(:item).permit(:name, :description, :price, :image)
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :photo, :user_id)
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+user1 = User.create(name: "user1")
+
+item1 = Item.create(name: "item1", user_id: user1.id, photo: "http/photo.com", description: "description1", specs: "specs1", price: "12")
+
+reserveds1 = Reserved.create(name: "reserved1", user_id: user1.id, item_id: item1.id, city: "city1", started_at: "2020-04-29", ended_at: "2020-04-29")
