ff365abfe176402500648687468b99e40a00cc79
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-05-02 17:21:23 +0200

    Create Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      devise_for :users, controllers: {
+        registrations: 'api/v1/users/registrations',
+        sessions: 'api/v1/users/sessions'
+      }
+      
       resources :items ,only: [:index,:show,:edit,:create,:update,:destroy] do
         resources :reserveds, only: [:index,:show,:edit,:create,:update,:destroy]
       end
