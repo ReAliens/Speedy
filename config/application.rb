611a27d2f9671c433c38da678b82f0550136e7e3
Ahmed <ahmedreda152@gmail.com>
2022-05-03 02:55:08 +0200

    fix

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,7 @@ module Speedy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     # config.factory_bot.definition_file_paths = ["custom/factories"]
->>>>>>> a9d86bd5ea8877847fca55e89c73b536b5f8270e
 
 
     # This also configures session_options for use below
@@ -24,12 +20,9 @@ module Speedy
     config.middleware.use ActionDispatch::Cookies
 
     config.middleware.use config.session_store, config.session_options
-<<<<<<< HEAD
-=======
+
     # config.factory_bot.definition_file_paths = ["custom/factories"]
->>>>>>> ae446de83a0b345f59f4b981d3e78b6c80a0aaca
-=======
->>>>>>> a9d86bd5ea8877847fca55e89c73b536b5f8270e
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
