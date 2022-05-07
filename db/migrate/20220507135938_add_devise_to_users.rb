875de053a19d96efa4c90cc67ccedf9c77bfcd2c
Ahmed <ahmedreda152@gmail.com>
2022-05-07 20:33:22 +0200

    update schema file

--- db/migrate/20220507135938_add_devise_to_users.rb
+++ db/migrate/20220507135938_add_devise_to_users.rb
@@ -38,7 +38,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :name,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
