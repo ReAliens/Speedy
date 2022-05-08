9eb6e626de2b29498ac673dbaf1cdee528fb3105
Ahmed <ahmedreda152@gmail.com>
2022-05-08 09:24:39 +0200

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-  
+
   has_many :items, foreign_key: 'user_id', dependent: :destroy
   has_many :reserveds, foreign_key: 'user_id', dependent: :destroy
 
